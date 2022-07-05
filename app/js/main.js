7a703e121ee10e4a50d995bf70e232f3c795342e
skrega <andrievichregina@gmail.com>
2022-07-05 22:26:21 +1000

    fixed adaptive and add function

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,12 @@
 $(function () {
+    // Слайдер 
     $('.main-items').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
         autoplay: true,
+        speed: 800,
+        pauseOnHover: true,
         autoplaySpeed: 3000,
         prevArrow: '<button type="button" class="slick-prev main-slide-btn"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="20" transform="rotate(-180 20 20)" fill="#F8EDDF"/><path d="M20 12L12 20M12 20L20 28M12 20L28 20" stroke="#EC5A33" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
         nextArrow: '<button type="button" class="slick-next main-slide-btn"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="20" fill="#F8EDDF"/><path d="M20 28L28 20M28 20L20 12M28 20L12 20" stroke="#EC5A33" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
@@ -17,6 +20,7 @@ $(function () {
         ]
     });
 
+    // Validator инизиализация
     window.onload = () => {
         document.getElementById('form').addEventListener('submit', e => {
             let valid = new Validator('form');
@@ -26,10 +30,27 @@ $(function () {
         })
     }
 
+    // Popup
     Fancybox.bind("[data-fancybox]", {
         autoFocus: false,
     });
 
+    // Передача названия эксурсии в скрытый input
+    document.addEventListener("DOMContentLoaded", () => {
+        const buttons = document.querySelectorAll('.tour-item');
+        const excursion = document.getElementById('excursion')
+
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", () => {
+                const parent = buttons[i];
+                const title = parent.querySelector('.tour-item__heading').textContent;
+                excursion.setAttribute('value', title);
+
+            }, false);
+        }
+    });
+
+    // Отправка формы
     $('#form').on('submit', function () {
         if (document.form.name.value == '' && document.form.phone.value == '') {
             valid = false;
@@ -45,7 +66,7 @@ $(function () {
             data: $(this).serialize()
         }).done(function () {
 
-            Fancybox.close('[data-fancybox="form"]');
+            Fancybox.close('[data-fancybox]');
 
             Fancybox.show([{
                     src: "#popup-success",
@@ -61,6 +82,7 @@ $(function () {
         });
         return false;
     });
+
 
 
 
